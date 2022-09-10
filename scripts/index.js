cde36844784826aa19821d53dbcc3decace37e60
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-10 22:13:18 +0300

    adding the age api

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+//the purpose of these three variables is store their value globally
+//and use it among the execution
+var age;
+var gender;
+var nationality;
 const picture = document.getElementById("picture");
 const dogName = document.getElementById("dog-name");
 const predict = document.getElementById("predict");
@@ -6,6 +11,7 @@ const error = document.getElementById("error");
 error.style.display = "none";
 //this var is to track the presence of error message or not
 var state = false;
+//this regx is only for letters characters  no spaces
 const regName = /^[a-zA-Z]+$/;
 // this code will call the dog pic api
 //and get a random dog image from there
@@ -23,7 +29,9 @@ fetch("https://dog.ceo/api/breeds/image/random")
     picture.innerHTML = `<img src="${data_obj.message}"/>`;
   });
 
-//this will
+//this will listen the click of the predict button
+//and it will check for the validity of the name by regex
+//and if it passes it will call the function of generating the story
 predict.onclick = () => {
   if (state) {
     state = false;
@@ -33,5 +41,21 @@ predict.onclick = () => {
   if (!regName.test(name)) {
     state = true;
     error.style.display = "block";
+    return;
   }
+  generate_story(name);
 };
+
+//this function will generate the biography of the dog
+//by calling three apis age , gender and, nationality
+async function generate_story(name) {
+  // age api
+  await fetch("https://api.agify.io/?name=" + name)
+    .then((res) => res.text())
+    .then((data) => {
+      let data_obj = {};
+      data_obj = JSON.parse(data);
+      age = data_obj.age;
+      console.log(data_obj);
+    });
+}
