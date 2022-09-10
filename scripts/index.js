e500527c20ebf908159e3a7460aa8aececca5cf9
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-10 20:59:52 +0300

    adding an error message for wrong inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,12 @@
 const picture = document.getElementById("picture");
+const dogName = document.getElementById("dog-name");
+const predict = document.getElementById("predict");
+//an error message that will appear when invalid name is passed by client
+const error = document.getElementById("error");
+error.style.display = "none";
+//this var is to track the presence of error message or not
+var state = false;
+const regName = /^[a-zA-Z]+$/;
 // this code will call the dog pic api
 //and get a random dog image from there
 fetch("https://dog.ceo/api/breeds/image/random")
@@ -14,3 +22,16 @@ fetch("https://dog.ceo/api/breeds/image/random")
     console.log(data_obj);
     picture.innerHTML = `<img src="${data_obj.message}"/>`;
   });
+
+//this will
+predict.onclick = () => {
+  if (state) {
+    state = false;
+    error.style.display = "none";
+  }
+  let name = dogName.value;
+  if (!regName.test(name)) {
+    state = true;
+    error.style.display = "block";
+  }
+};
